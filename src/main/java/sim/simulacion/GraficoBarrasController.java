59ed5628d4725629ce03dea554e5d71df2fdea48
juani15151 <wfalconwf@gmail.com>
2018-04-02 18:45:26 -0300

    Prueba de concepto realizada y efectiva.

--- src/main/java/sim/simulacion/GraficoBarrasController.java
+++ src/main/java/sim/simulacion/GraficoBarrasController.java
@@ -5,18 +5,22 @@ package sim.simulacion;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import generadores.GeneradorJava;
 import generadores.GeneradorUniforme;
 import generadores.IGenerador;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.XYChart;
 import pruebas.PruebaChiCuadrado;
+import javafx.collections.FXCollections;
 import pruebas.PruebaChiCuadradoUniforme;
 
 /**
@@ -27,38 +31,50 @@ import pruebas.PruebaChiCuadradoUniforme;
 public class GraficoBarrasController implements Initializable {
 
     @FXML
-    private BarChart<Integer, Integer> chart;
+    private BarChart<String, Integer> chart;
+    @FXML
+    private CategoryAxis chartXAxis;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        List<String> labelList = new ArrayList(10);
+        for (int i = 0; i < 10; i++) {
+            labelList.add(String.valueOf(i));
+        }
+
+        ObservableList<String> labels = FXCollections.observableArrayList();
+        labels.addAll(labelList);
+
+        // Assign the month names as categories for the horizontal axis.
+        chartXAxis.setCategories(labels);
+    }
 
     @FXML
     private void generateFromGenerador(ActionEvent event) {
-        this.generate(new GeneradorUniforme(100));
+        this.generate(new GeneradorUniforme());
     }
 
     @FXML
     private void generateFromRandom(ActionEvent event) {
-        this.generate(new GeneradorJava(100l));
+        this.generate(new GeneradorJava());
     }
-    
-    private void generate(IGenerador generador){
-        PruebaChiCuadrado test = new PruebaChiCuadradoUniforme(generador, 10);
-        int[] frecuencias = test.observarFrecuenciasPorIntervalo();       
-        
-        XYChart.Series<Integer, Integer> barras = new XYChart.Series<>();
-        
-        for(int i = 0; i < 10; i++){
 
-            barras.getData().add(new XYChart.Data<>(0, frecuencias[i]));
-        }
+    private void generate(IGenerador generador) {
         
+        PruebaChiCuadrado test = new PruebaChiCuadradoUniforme(generador, 10);
+        int[] frecuencias = test.observarFrecuenciasPorIntervalo();
+
+        XYChart.Series<String, Integer> barras = new XYChart.Series<>();
+
+        for (int i = 0; i < 10; i++) {
+
+            barras.getData().add(new XYChart.Data<>(String.valueOf(i), frecuencias[i]));
+        }
+
         chart.getData().add(barras);
     }
-    
+
 }
