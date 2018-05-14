ef2f345cf2ea9423ef53c34208f386c5ac19d35c
juani15151 <wfalconwf@gmail.com>
2018-05-14 14:59:05 -0300

    ConsoleMode eliminado.
    No se usaba desde hace mucho.

--- src/main/java/sim/simulacion/MainApp.java
+++ src/main/java/sim/simulacion/MainApp.java
@@ -19,60 +19,14 @@ public class MainApp extends Application {
 
         // Codigo por defecto del proyecto JavaFX. Se usa para ventanas. 
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Menu.fxml"));
-        
+
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
-        
+
         stage.setTitle("Trabajo Practico 1");
         stage.setScene(scene);
         stage.show();
         // */
-    }
-
-    /**
-     * Ejecutar el programa en consola hasta que tengamos interfaz grafica.
-     */
-    public void consoleMode() {
-        Scanner sc = new Scanner(System.in);
-        int semilla, a, c = 0, m;
-
-        // Configurar generador.
-        System.out.println("Seleccione el generador a usar:");
-        System.out.println("1- Generador Uniforme por metodo congruencial mixto.");
-        System.out.println("2- Generador Uniforme por metodo congruencial.");
-        int generador_choice = sc.nextInt();
-
-        System.out.print("Ingrese el valor semilla:");
-        semilla = sc.nextInt();
-        System.out.print("Ingrese la constante multiplicativa A:");
-        a = sc.nextInt();
-        System.out.print("Ingrese el valor del modulo M:");
-        m = sc.nextInt();
-
-        if (generador_choice == 1) {
-            System.out.print("Ingrese la constante aditiva C:");
-            c = sc.nextInt();
-        } else {
-            c = 0;
-        }
-        
-        GeneradorUniforme generador = new GeneradorUniforme(semilla, a, c, m,0,1);      
-
-        // Mostrar primeros 20 valores.
-        System.out.println("Primeros 20 valores: ");
-        DecimalFormat df = new DecimalFormat();
-        for (int i = 0; i < 20; i++) {
-            System.out.print(Round.truncate(generador.nextDouble(), 4));
-            System.out.print(", ");
-        }
-        System.out.println("");
-
-        // Mostrar los siguientes de a uno.
-        System.out.println("Presione enter para ver los siguientes valores uno a uno.");
-        while (true) {
-            sc.nextLine();
-            System.out.print(Round.truncate(generador.nextDouble(), m));
-        }
     }
 
     /**
