c5e33a7a399658df1d0e5dac5356330b1960c2b6
Octavio <31598015+octa351@users.noreply.github.com>
2018-05-16 23:11:36 -0300

    Terminado

--- src/main/java/montecarlo/SobreVentaPasajes.java
+++ src/main/java/montecarlo/SobreVentaPasajes.java
@@ -36,21 +36,93 @@ public class SobreVentaPasajes {
         utilidadAcumulada = 0;
     }
 
+    
+    public int getCapacidadVuelo() {        
+        return capacidadVuelo;
+    }
+
+    public void setCapacidadVuelo(int capacidadVuelo) {
+        this.capacidadVuelo = capacidadVuelo;
+    }
+
+    public IGeneradorPasajeros getGeneradorPasajeros() {
+        return generadorPasajeros;
+    }
+
+    public void setGeneradorPasajeros(IGeneradorPasajeros generadorPasajeros) {
+        this.generadorPasajeros = generadorPasajeros;
+    }
+
+    public double getUtilidadPorPasajero() {
+        return utilidadPorPasajero;
+    }
+
+    public void setUtilidadPorPasajero(double utilidadPorPasajero) {
+        this.utilidadPorPasajero = utilidadPorPasajero;
+    }
+
+    public double getCostoSobreVenta() {
+        return costoSobreVenta;
+    }
+
+    public void setCostoSobreVenta(double costoSobreVenta) {
+        this.costoSobreVenta = costoSobreVenta;
+    }
+
+    public int getReloj() {
+        return reloj;
+    }
+
+    public void setReloj(int reloj) {
+        this.reloj = reloj;
+    }
+
+    public double getRndCantidadPasajeros() {
+        return rndCantidadPasajeros;
+    }
+
+    public void setRndCantidadPasajeros(double rndCantidadPasajeros) {
+        this.rndCantidadPasajeros = rndCantidadPasajeros;
+    }
+
+    public int getCantidadPasajeros() {
+        return cantidadPasajeros;
+    }
+
+    public void setCantidadPasajeros(int cantidadPasajeros) {
+        this.cantidadPasajeros = cantidadPasajeros;
+    }
+
+    public double getUtilidad() {
+        return utilidad;
+    }
+
+    public void setUtilidad(double utilidad) {
+        this.utilidad = utilidad;
+    }
+
+    public double getUtilidadAcumulada() {
+        return utilidadAcumulada;
+    }
+
     /**
      * Avanza el reloj en 'n' iteraciones.
      * @param n: cantidad de iteraciones a realizar.
      */
-    public void iterar(int n) {
-        for (; n >= 0; n--) {
-            iterar();
-        }
+//    public void iterar(int n) {
+//        for (; n >= 0; n--) {
+//            iterar();
+//        }
+//    }
+    public void setUtilidadAcumulada(double utilidadAcumulada) {
+        this.utilidadAcumulada = utilidadAcumulada;
     }
 
     /**
      * Avanza el reloj en 1.
      * Se calcula todo lo correspondiente a esa fila.
      */
-    public void iterar() {        
+    public void iterar() {
         reloj++;        
         // Cantidad de pasajeros.
         cantidadPasajeros = generadorPasajeros.nextCantidadPasajeros();
--- src/main/java/sim/simulacion/MontecarloController.java
+++ src/main/java/sim/simulacion/MontecarloController.java
@@ -8,10 +8,21 @@ package sim.simulacion;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javax.swing.JOptionPane;
 import montecarlo.SobreVentaPasajes;
-import montecarlo.generadoresPasajeros.Generador32Reservas;
+import montecarlo.generadoresPasajeros.*;
 import montecarlo.generadoresPasajeros.IGeneradorPasajeros;
+import utils.Round;
 
 /**
  * FXML Controller class
@@ -21,16 +32,32 @@ import montecarlo.generadoresPasajeros.IGeneradorPasajeros;
 public class MontecarloController implements Initializable {
 
     
-    private int cantidadIteraciones;
-    private int graficarDesde;
-    private int graficarHasta;
+    @FXML
+    private TextField txt_n;
+    @FXML
+    private TextField txt_desde;
+    @FXML
+    private TextField txt_hasta;
+    @FXML
+    private TextField txt_promedio;
+    @FXML
+    private ComboBox<String> cmb_generadores;
+    @FXML
+    private Button btn_simular;
+    @FXML
+    private ListView<String> list_Vector;
+     private ObservableList<String> vectorEstado = FXCollections.observableArrayList();
     
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        cmb_generadores.getItems().add("31");
+        cmb_generadores.getItems().add("32");
+        cmb_generadores.getItems().add("33");
+        cmb_generadores.getItems().add("34");
+        
     }    
     
     
@@ -39,30 +66,82 @@ public class MontecarloController implements Initializable {
      * @return 
      */
     private IGeneradorPasajeros getGenerador(){
-        // TODO
-        // Normalmente este metodo debe devolver el valor elegido en un
-        // combo de generadores (Gen 31, 32, 33 o 34).
-        return new Generador32Reservas();
+        if(cmb_generadores.getItems().get(0).equals(cmb_generadores.getValue())){
+            return new Generador31Reservas();
+        }
+        if(cmb_generadores.getItems().get(1).equals(cmb_generadores.getValue())){
+            return new Generador32Reservas();
+        }
+        if(cmb_generadores.getItems().get(2).equals(cmb_generadores.getValue())){
+            return new Generador33Reservas();
+        }
+        if(cmb_generadores.getItems().get(3).equals(cmb_generadores.getValue())){
+            return new Generador34Reservas();
+        }
+        return null;
     }
     
+    @FXML
     public void run(){
+        try{
+        if(validarNoNegativo()){
+            if(validardesdeHasta()){
         reset();
         SobreVentaPasajes simulacion = new SobreVentaPasajes(getGenerador());
-        // Iteramos hasta el primero que hay que graficar.
-        simulacion.iterar(graficarDesde - 1);
-        for(int i = graficarDesde; i < graficarHasta; i++){
-            simulacion.iterar();
-            // TODO: Graficar estado.
-            simulacion.getEstado();
+        int n = Integer.parseInt(txt_n.getText());
+        String cad = "";
+        for (; n > 0; n--) {
+        simulacion.iterar();
+        if(simulacion.getReloj() >= Integer.parseInt(txt_desde.getText()) && simulacion.getReloj() <= Integer.parseInt(txt_hasta.getText()) ){
+          cad = "Reloj: " + simulacion.getReloj() + " - RND: " + Round.truncate(simulacion.getRndCantidadPasajeros(), 2) + " - Cantidad Pasajeros: " + Round.truncate(simulacion.getCantidadPasajeros(),2) + " - Utilidad: " + Round.truncate(simulacion.getUtilidad(),2) + " - Utilidad Acumulada: " + Round.truncate(simulacion.getUtilidadAcumulada(), 2);   
+          vectorEstado.add(cad);
+        }     
+    }
+        list_Vector.getItems().addAll(vectorEstado); 
+        txt_promedio.setText(String.valueOf(Round.truncate(simulacion.utilidadPromedio(), 2)));
+
+        }
+        else{
+        JOptionPane.showMessageDialog(null, "ERROR, DESDE ES MENOR QUE HASTA Y VISCEVERSA");
+        }
+        }
+        else{
+            
+            JOptionPane.showMessageDialog(null, "ERROR, NO INGRESE DATOS NEGATIVOS");
+        }
+        }
+        catch(NullPointerException ex){
+            JOptionPane.showMessageDialog(null, "ERROR, SELECCIONE UN VALOR EN EL COMBO");
         }
     }
+  
+    
+    
     
     /**
      * Reinicia la tabla y todos los controles necesarios para el inicio de una
      * simulacion.
      */
     private void reset(){
-        // TODO: Reiniciar tabla.
+        list_Vector.getItems().clear();
+        vectorEstado.clear();
     }
     
+   private boolean validardesdeHasta(){
+       return Integer.parseInt(txt_desde.getText()) < Integer.parseInt(txt_hasta.getText());
+   }
+   
+   private boolean validarNoNegativo(){
+       return Integer.parseInt(txt_desde.getText()) >= 0 && Integer.parseInt(txt_hasta.getText()) >= 0 && Integer.parseInt(txt_n.getText()) > 0;
+   }
+   
+   private boolean validarSeleccionCombo(){
+       return cmb_generadores.getValue() != "";
+   }
+   
+   private void limpiarCampos(){
+       txt_desde.setText("0");
+       txt_hasta.setText("0");
+       txt_n.setText("1");
+   }
 }
