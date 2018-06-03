effaf8441a66ffcac2a51bfd6f7c89166707c763
juani15151 <wfalconwf@gmail.com>
2018-06-03 14:53:01 -0300

    Los generadores se instancian en el new.

--- src/main/java/colas/Prueba.java
+++ src/main/java/colas/Prueba.java
@@ -4,95 +4,94 @@
  * and open the template in the editor.
  */
 package colas;
+
 import generadores.*;
 import java.util.LinkedList;
+
 /**
  *
  * @author eric
  */
 public class Prueba {
-    private double reloj;
+
     private int numeroPruebas;
+    private double reloj;
+
+    private IGenerador generadorProximaLlegada;
     private double proximaLlegada;
-    private double finAtFiambreria;
-    private double finAtCarniceria;
+    private IGenerador generadorFinAtencionFiambreria;
+    private Double finAtFiambreria;
+    private IGenerador generadorFinAtencionCarniceria;
+    private Double finAtCarniceria;
     private Servidor svFiambreria;
     private Servidor svCarniceria;
 
     public Prueba(int numeroPruebas) {
-        this.reloj = 0;
         this.numeroPruebas = numeroPruebas;
-        this.proximaLlegada = 0;
-        this.finAtFiambreria = 0;
-        this.finAtCarniceria = 0;
-        this.svCarniceria = new Servidor("Carniceria","Libre");
-        this.svFiambreria = new Servidor("Fiambreria","Libre");
+        this.reloj = 0;
+
+        this.generadorProximaLlegada = new GeneradorExponencial(0.5);
+        this.proximaLlegada = generadorProximaLlegada.nextDouble();
+        this.generadorFinAtencionFiambreria = new GeneradorUniformePersonalizado(1, 3);
+        this.finAtFiambreria = null;
+        this.generadorFinAtencionCarniceria = new GeneradorUniformePersonalizado(0.5, 2.5);
+        this.finAtCarniceria = null;
+        this.svCarniceria = new Servidor("Carniceria", "Libre");
+        this.svFiambreria = new Servidor("Fiambreria", "Libre");
     }
 
     //esta es una especie de main que hace las simulaciones, le falta para contemplar todos los casos
-    public void linea()
-    {
+    public void linea() {
         //Pense en crear los generadores fuera del ciclo, pero dejo adentro para que entiendan mejor el codigo
         //GeneradorExponencial generadorExp = new GeneradorExponencial(0.5);
         //GeneradorUniformePersonalizado genFiambreria = new GeneradorUniformePersonalizado(1,3);
         //GeneradorUniformePersonalizado genCarniceria = new GeneradorUniformePersonalizado(0.5,2.5);
-        
+
         for (int i = 0; i < numeroPruebas; i++) {
             //cuando el reloj esta en 0 no ejecutamos los fines de atencion para que no se atienda al cliente
 
-            if (proximaLlegada == reloj)
-            {
+            if (proximaLlegada == reloj) {
                 llegaCliente();
             }
-            if (finAtFiambreria == reloj && reloj != 0)
-                //Evento fin atencion fiambreria
+            if (finAtFiambreria == reloj && reloj != 0) //Evento fin atencion fiambreria
             {
-                atenderFiambreria(); 
+                atenderFiambreria();
             }
-            if (finAtCarniceria == reloj && reloj != 0)
-            {
+            if (finAtCarniceria == reloj && reloj != 0) {
                 atenderCarniceria();
             }
-            //para el proximo evento ponemos el reloj en el menor de los 3
-            reloj = Math.min(proximaLlegada, finAtFiambreria) ;
+            //para el proximo evento ponemos el reloj en el menor de los 3            
+            reloj = Math.min(proximaLlegada, finAtFiambreria);
             reloj = Math.min(reloj, finAtCarniceria);
         }
     }
-    
-    private void llegaCliente()
-    {
+
+    private void llegaCliente() {
         {
-                //Evento Llegada Cliente
-                GeneradorExponencial generadorExp = new GeneradorExponencial(0.5);
-                Evento eventoLlegada = new Evento("Llegada Cliente",reloj ,generadorExp);
-                //El tiempo entre llegada del evento llegada cliente
-                eventoLlegada.getTiempoDuracion();
-                //El tiempo de la proxima llegada
-                this.proximaLlegada = eventoLlegada.getTiempoFin();
-                //Al principio de la simulacion siempre el evento es la proxima
-                //llegada de cliente, asi que no hay un cliente antes
-                if (reloj != 0)
-                {
-                    //Creacion de el cliente que recien llega
-                    Cliente nuevoCliente = new Cliente(reloj);
-                    //Lo ingresamos a la cola que le corresponda
-                    meterClienteACola(nuevoCliente);
-                }
+            //Evento Llegada Cliente
+            Evento eventoLlegada = new Evento("Llegada Cliente", reloj, this.generadorProximaLlegada);
+            //El tiempo entre llegada del evento llegada cliente
+            eventoLlegada.getTiempoDuracion();
+            //El tiempo de la proxima llegada
+            this.proximaLlegada = eventoLlegada.getTiempoFin();
+            //Al principio de la simulacion siempre el evento es la proxima
+            //llegada de cliente, asi que no hay un cliente antes
+            if (reloj != 0) {
+                //Creacion de el cliente que recien llega
+                Cliente nuevoCliente = new Cliente(reloj);
+                //Lo ingresamos a la cola que le corresponda
+                meterClienteACola(nuevoCliente);
+            }
         }
     }
-    
-    private void atenderFiambreria()
-    {
-        if (svFiambreria.cola.isEmpty())
-        {
+
+    private void atenderFiambreria() {
+        if (svFiambreria.cola.isEmpty()) {
             //se termino de atender y no hay mas clientes en la cola
             svFiambreria.setEstado("Libre");
-        }
-        else
-            //se termino de atender y hay clientes en la cola
+        } else //se termino de atender y hay clientes en la cola
         {
-            GeneradorUniformePersonalizado genFiambreria = new GeneradorUniformePersonalizado(1,3);
-            Evento eventoFinAtFiambreria = new Evento("Fin de atencion Fiambreria", reloj, genFiambreria);
+            Evento eventoFinAtFiambreria = new Evento("Fin de atencion Fiambreria", reloj, this.generadorFinAtencionFiambreria);
             //tiempo de atencion
             eventoFinAtFiambreria.getTiempoDuracion();
             //cliente que se atiende
@@ -103,29 +102,22 @@ public class Prueba {
             svFiambreria.acumularTiempo(tiempoEspera);
             //tiempo final
             finAtFiambreria = eventoFinAtFiambreria.getTiempoFin();
-            if(clienteEnAtencion.tieneVerdura())
-            {
+            if (clienteEnAtencion.tieneVerdura()) {
                 finAtFiambreria += 0.2;
             }
-                    
+
         }
-                
+
     }
-    
-    
-    private void atenderCarniceria()
-    {
+
+    private void atenderCarniceria() {
         //Evento fin atencion Carniceria
-        if (svCarniceria.cola.isEmpty())
-        {
+        if (svCarniceria.cola.isEmpty()) {
             //se termino de atender y no hay mas clientes en la cola
             svCarniceria.setEstado("Libre");
-        }
-        else
-            //se termino de atender y hay clientes en la cola
+        } else //se termino de atender y hay clientes en la cola
         {
-            GeneradorUniformePersonalizado genCarniceria = new GeneradorUniformePersonalizado(0.5,2.5);
-            Evento eventoFinAtCarniceria = new Evento("Fin de atencion Carniceria", reloj, genCarniceria);
+            Evento eventoFinAtCarniceria = new Evento("Fin de atencion Carniceria", reloj, this.generadorFinAtencionCarniceria);
             //tiempo de atencion
             eventoFinAtCarniceria.getTiempoDuracion();
             //cliente que se atiende
@@ -136,35 +128,27 @@ public class Prueba {
             svCarniceria.acumularTiempo(tiempoEspera);
             //tiempo final
             finAtCarniceria = eventoFinAtCarniceria.getTiempoFin();
-            if(clienteEnAtencion.tieneVerdura())
-            {
+            if (clienteEnAtencion.tieneVerdura()) {
                 finAtCarniceria += 0.2;
             }
         }
     }
-    
-    private void meterClienteACola(Cliente cliente){
-                //primero vemos si es de verduleria
-                if ("Verduleria".equals(cliente.getTipoCliente()) ){
-                    //si es de verdulera entra en la cola mas corta
-                    if (svFiambreria.cola.size() <= svCarniceria.cola.size())
-                    {
-                        svFiambreria.cola.add(cliente);
-                    }
-                    else 
-                    {
-                        svCarniceria.cola.add(cliente);
-                    }
-                }
-                else if ("Fiambreria".equals(cliente.getTipoCliente())) {
-                    svFiambreria.cola.add(cliente);
-                }
-                // si no es de verduleria ni fiambreria entonces es de carniceria
-                else
-                {
-                    svCarniceria.cola.add(cliente);
-                }
+
+    private void meterClienteACola(Cliente cliente) {
+        //primero vemos si es de verduleria
+        if ("Verduleria".equals(cliente.getTipoCliente())) {
+            //si es de verdulera entra en la cola mas corta
+            if (svFiambreria.cola.size() <= svCarniceria.cola.size()) {
+                svFiambreria.cola.add(cliente);
+            } else {
+                svCarniceria.cola.add(cliente);
+            }
+        } else if ("Fiambreria".equals(cliente.getTipoCliente())) {
+            svFiambreria.cola.add(cliente);
+        } // si no es de verduleria ni fiambreria entonces es de carniceria
+        else {
+            svCarniceria.cola.add(cliente);
+        }
     }
-    
-    
+
 }
